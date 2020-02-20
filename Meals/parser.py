e2d7b4e70aac7ab7f85b04ae8f2af6805da2f3c3
parasg <parasgaba123@gmail.com>
2020-02-19 20:42:27 -0600

    adding parser.py, which scrapes the API for all meals, and a cache for the meals data

---
+++ Meals/parser.py
@@ -0,0 +1,20 @@
+#parsing 
+import requests
+
+def parse_meals():
+	meals= []
+	alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+	url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+	for i in range(1,26):
+		r = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+		d = r.json()
+		meal_list = []
+		d1 = {}
+		for key in d:
+			meal_list = d[key]
+		for dicts in meal_list:
+			meals.append(dicts)
+		url = url[0:len(url)-1] + alph[i]
+	print((meals))
+	return meals
+parse_meals()
