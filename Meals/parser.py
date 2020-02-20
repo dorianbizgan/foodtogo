2cfb8d9d2f769c49c3c02e4e65a9f28cc6605fb5
parasg <parasgaba123@gmail.com>
2020-02-19 21:01:30 -0600

    fixed parser to get meals from all alphabets

--- Meals/parser.py
+++ Meals/parser.py
@@ -6,15 +6,18 @@ def parse_meals():
 	alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 	url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
 	for i in range(1,26):
-		r = requests.get('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+		r = requests.get(url)
 		d = r.json()
 		meal_list = []
 		d1 = {}
 		for key in d:
 			meal_list = d[key]
-		for dicts in meal_list:
-			meals.append(dicts)
+		try:
+			for dicts in meal_list:
+				meals.append(dicts)
+		except:
+			pass
 		url = url[0:len(url)-1] + alph[i]
-	print((meals))
+	print(meals)
 	return meals
 parse_meals()
