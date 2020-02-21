3a3ee42675a2db500bbd435004586176bd67e227
Dorian Bizgan <dorianbizgan@gmail.com>
2020-02-20 21:18:31 -0600

    changed bootstrap.html to index.html and changed route to reflect

--- Meals/main.py
+++ Meals/main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("bootstrap.html")
+    return render_template("index.html")
 
 @app.route('/about-us/')
 def aboutUs():
